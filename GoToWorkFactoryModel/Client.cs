2c69c4f770707a5efafcfc94b6ebb8655501b723
Sansiel <deviler.san@yandex.ru>
2019-04-21 00:02:39 +0400

    Add model

---
+++ GoToWorkFactoryModel/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace GoToWorkFactoryModel
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string ClientFname { get; set; }
+        public string ClientLname { get; set; }
+        public string ClientMname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
--- GoToWorkFactoryModel/OrderProduct.cs
+++ GoToWorkFactoryModel/OrderProduct.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GoToWorkFactoryModel
 {
-    public class Class1
+    class OrderProduct
     {
     }
 }
